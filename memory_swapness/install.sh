5d83f8be9c2bcd271d1332be57cfae918d892b08
Erick Lemos <ericklemoscosta@gmail.com>
2024-05-03 12:20:46 -0300

    ajuste gerais

--- memory_swapness/install.sh
+++ memory_swapness/install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+sudo tee -a /etc/sysctl.d/99-sysctl.conf <<-EOF  
+
+EOF
+
 sudo tee -a /etc/sysctl.d/99-sysctl.conf <<-EOF
-vm.swappiness=1
+vm.swappiness=10
 vm.vfs_cache_pressure=50
 EOF
 
-sudo tee -a /etc/sysctl.d/99-sysctl.conf <<-EOF 
-vm.dirty_background_bytes=16777216 
-vm.dirty_bytes=50331648 
+sudo tee -a /etc/sysctl.d/99-sysctl.conf <<-EOF  
+
+EOF
+
+sudo tee -a /etc/sysctl.d/99-sysctl.conf <<-EOF  
+vm.dirty_background_ratio=5
+vm.dirty_ratio=10 
 EOF
