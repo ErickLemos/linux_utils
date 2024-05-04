9bb8daa63896d97e1ff584363258f97933a8e8ea
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-05-04 17:35:19 -0300

    ajustes gerais

--- software_installs/install-essential-packages.sh
+++ software_installs/install-essential-packages.sh
@@ -8,8 +8,12 @@ sudo apt-get install git -y
 sudo apt-get install ffmpeg -y
 sudo apt-get install libfuse2 -y
 sudo apt-get install synaptic -y
+
+### python env with cuda utils ###
 sudo apt-get install nvidia-cuda-toolkit -y
 sudo apt-get install nvidia-cudnn -y
+
+### utils midia softwares ###
 sudo apt-get install gimp -y
 sudo apt-get install vlc -y
 
