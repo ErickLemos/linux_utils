251af1e3b2cace20df50dbced72086c04b4f8a12
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-05-04 15:51:48 -0300

    essential packages: vlc

--- software_installs/install-essential-packages.sh
+++ software_installs/install-essential-packages.sh
@@ -9,3 +9,5 @@ sudo apt-get install ffmpeg -y
 sudo apt-get install libfuse2 -y
 sudo apt-get install synaptic -y
 sudo apt-get install nvidia-cuda-toolkit -y
+sudo apt-get install gimp -y
+sudo apt-get install vlc -y
