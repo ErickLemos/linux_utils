89dafa07fd24aab1baa99bd4d6a2e9301ea66a92
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-05-05 13:07:00 -0300

    vpn remove

--- software_installs/vpn_wireguard/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt install wireguard -y
-sudo mkdir -p /etc/wireguard/keys; wg genkey | sudo tee /etc/wireguard/keys/server.key | wg pubkey | sudo tee /etc/wireguard/keys/server.key.pub
-sudo cat /etc/wireguard/keys/server.key
-
-# configurar para iniciar junto com o sistema
-sudo systemctl enable wg-quick@wg0
--- software_installs/vpn_wireguard/up-vpn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo wg-quick up wg0
-sudo wg show wg0
-
-##
-sudo ufw allow 51820/udp
-sudo ufw enable
