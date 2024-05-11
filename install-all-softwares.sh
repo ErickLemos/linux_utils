1f2d703086aec2fad8599bd23916feb1e797db10
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-05-11 15:56:33 -0300

    essentials packages

--- install-all-softwares.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# executa todos os scripts da pasta /software_installs
-
-software_installs_dir=./software_installs
-source $software_installs_dir/install-essential-packages.sh
-source $software_installs_dir/install-flathub-base.sh
-source $software_installs_dir/install-graalvm.sh
-source $software_installs_dir/install-rust.sh
