16c491d269ed12f6fc62a2d11da9c2c09c347caf
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-05-11 16:18:42 -0300

    essential softwares: google chrome

--- configure-gnome.sh
+++ configure-gnome.sh
@@ -8,6 +8,7 @@ gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-delay 0.1
 
 # background
 gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/Little_numbat_boy_by_azskalt.png"
