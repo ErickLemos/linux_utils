90a4c4713cc9984afc47e4d671aa56362f308714
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-05-13 12:06:33 -0300

    ajustes gerais

--- configure-gnome.sh
+++ configure-gnome.sh
@@ -9,7 +9,6 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 gsettings set org.gnome.shell.extensions.dash-to-dock show-delay 0.1
-gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color true
 
 # background
 gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/Little_numbat_boy_by_azskalt.png"
